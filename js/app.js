540b7405b2ec2c7e4e3d8477bb695f0d08d67678
Sourav <souravrkl.gamefreak@gmail.com>
2017-10-02 15:51:16 +0530

    [ADD] http request

--- js/app.js
+++ js/app.js
@@ -1,27 +1,11 @@
 (function (window, document, undefined) {
 
-    const questions = [
-        { id: 1, title: 'Your name', type: 'text', mandatory: true, has_options: false },
-        { id: 2, title: 'Your email', type: 'email', mandatory: true, has_options: false },
-        { id: 3, title: 'Your contact number', type: 'number', mandatory: true, has_options: false },
-        { id: 4, title: 'How likely is that you recommend this survey to a friend or a colleague?', type: 'rating', mandatory: true, has_options: false },
-        { id: 5, title: 'Overall how satisfied or dissatisfied are you with this survey?', type: 'radio', mandatory: true, has_options: true, options: [
-            { value: 'satisfied', label: 'Satisfied' },
-            { value: 'somewhat_safisfied', label: 'Somewhat satisfied' },
-            { value: 'dissatisfied', label: 'Dissatisfied' },
-            { value: 'somewhat_dissafisfied', label: 'Somewhat dissatisfied' },
-            { value: 'neither', label: 'Neither satisfied not dissatisfied' }
-        ] },
-        { id: 6, title: 'Which of the following words would you use to describe our product? Select all that apply', type: 'checkbox', mandatory: true, has_options: true, options: [
-            { value: 'reliable', label: 'Reliable' },
-            { value: 'useful', label: 'Useful' },
-            { value: 'unique', label: 'Unique' },
-            { value: 'overpriced', label: 'Overpriced' },
-            { value: 'impratical', label: 'Impractical' },
-            { value: 'ineffective', label: 'Ineffective' },
-        ] },
-        { id: 7, title: 'Do you have any comments or concerns?', type: 'textarea', mandatory: false, has_options: false },
-    ];
+    const serverURL = '//localhost';
+    const port = '8000';
+
+    const serverFullURL = serverURL + ':' + port;
+    const endpoint = '/questions';
+    const optionsEndpoint = '/options?question=';
 
     const mainWrapper = document.getElementsByClassName('main-content');
     const link = document.createElement('link');
@@ -29,10 +13,34 @@
     link.setAttribute('href', '../components/dv-card.html');
     link.onload = function () {
         // do stuff with import content
-        addQuestions(questions);
-        addSubmitBtn();
+        getHttpRequest(serverFullURL + endpoint, function(response) {
+            let allQuestions = JSON.parse(response);
+            allQuestions.forEach((question, idx) => {
+                if (question['has_options']) {
+                    getHttpRequest(serverFullURL + optionsEndpoint + question['id'], function(optionsResponse) {
+                        allQuestions[idx]['options'] = JSON.parse(optionsResponse);
+
+                        if (question['id'] === 6) {
+                            addQuestions(allQuestions);
+                            addSubmitBtn();
+                        }
+                    });
+                }
+            });
+        });
     };
     document.body.appendChild(link);
+
+    function getHttpRequest(url, callback) {
+        const xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                callback(xmlHttp.responseText);
+            }
+        };
+        xmlHttp.open('GET', url, true);
+        xmlHttp.send(null);
+    }
 
     function addQuestions(questions) {
         questions.forEach((question) => {
